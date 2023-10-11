0930d48c7903af018ffc288f299bd56b2e3039d8
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-11 16:09:01 -0300

    feat: :lipstick: Album styles.

--- src/components/Album/Album.tsx
+++ src/components/Album/Album.tsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
 import TrackList from "../TrackList";
 import { IAlbum } from "../../interfaces/album";
 import styles from "./Album.module.scss";
@@ -14,13 +12,9 @@ const Album = ({
   tracks,
   handlePlayClick,
   handleTrackPlayClick,
-  handleAlbumPlusClick,
-  handleAlbumMinusClick,
   showTracks,
   albumNameToShowTracks,
 }: IAlbum) => {
-  const [plusSize, setPlusSize] = useState("1.0em");
-
   return (
     <div className={styles.album}>
       <div className={styles.albumData}>
@@ -33,35 +27,6 @@ const Album = ({
         >
           {name}
         </a>
-        {showTracks && albumNameToShowTracks === name ? (
-          <FaMinusCircle
-            color="white"
-            onMouseEnter={() => {
-              setPlusSize("1.1em");
-            }}
-            onMouseLeave={() => {
-              setPlusSize("1.0em");
-            }}
-            size={plusSize}
-            onClick={() => {
-              handleAlbumMinusClick(name);
-            }}
-          />
-        ) : (
-          <FaPlusCircle
-            color="white"
-            onMouseEnter={() => {
-              setPlusSize("1.1em");
-            }}
-            onMouseLeave={() => {
-              setPlusSize("1.0em");
-            }}
-            size={plusSize}
-            onClick={() => {
-              handleAlbumPlusClick(name);
-            }}
-          />
-        )}
       </div>
       <Separator color="white" className={styles.separator} />
       {showTracks && albumNameToShowTracks === name && (
