bb4658113c285c0005c10039a5314eb846991231
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-11 15:18:52 -0300

    feat: :lipstick: Added AlbumsList styles.

--- src/components/AlbumsList/AlbumsList.tsx
+++ src/components/AlbumsList/AlbumsList.tsx
@@ -1,6 +1,7 @@
 import { IAlbum } from "interfaces/album";
 import Album from "../Album";
 import { IAlbumsList } from "interfaces/albumsList";
+import styles from "./AlbumsList.module.scss";
 
 const AlbumsList = ({
   returnedArtistData,
@@ -12,29 +13,31 @@ const AlbumsList = ({
   albumNameToShowTracks,
 }: IAlbumsList) => {
   return (
-    <ul>
-      {returnedArtistData
-        .filter((album: IAlbum) => album.tracks.length !== 0)
-        .map((album: IAlbum) => (
-          <li>
-            <Album
-              key={album.id}
-              name={album.name}
-              url={album.url}
-              image={album.image}
-              id={album.id}
-              artist={album.artist}
-              tracks={album.tracks}
-              handlePlayClick={handlePlayClick}
-              handleTrackPlayClick={handleTrackPlayClick}
-              handleAlbumPlusClick={handleAlbumPlusClick}
-              handleAlbumMinusClick={handleAlbumMinusClick}
-              showTracks={showTracks}
-              albumNameToShowTracks={albumNameToShowTracks}
-            />
-          </li>
-        ))}
-    </ul>
+    <div className={styles.albumsList}>
+      <ul>
+        {returnedArtistData
+          .filter((album: IAlbum) => album.tracks.length !== 0)
+          .map((album: IAlbum) => (
+            <li>
+              <Album
+                key={album.id}
+                name={album.name}
+                url={album.url}
+                image={album.image}
+                id={album.id}
+                artist={album.artist}
+                tracks={album.tracks}
+                handlePlayClick={handlePlayClick}
+                handleTrackPlayClick={handleTrackPlayClick}
+                handleAlbumPlusClick={handleAlbumPlusClick}
+                handleAlbumMinusClick={handleAlbumMinusClick}
+                showTracks={showTracks}
+                albumNameToShowTracks={albumNameToShowTracks}
+              />
+            </li>
+          ))}
+      </ul>
+    </div>
   );
 };
 
