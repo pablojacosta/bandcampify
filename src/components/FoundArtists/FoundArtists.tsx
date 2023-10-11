47bd5eccd9d90d5b628cd210ba9641585f87d5d5
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-11 13:21:00 -0300

    feat: :sparkles: Created useGetArtistAlbums hook. REfactored FoundArtists component.

--- src/components/FoundArtists/FoundArtists.tsx
+++ src/components/FoundArtists/FoundArtists.tsx
@@ -1,37 +1,9 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import useGetArtistAlbums from "@hooks/useGetArtistAlbums";
 import styles from "./FoundArtists.module.scss";
-import { useSelectedArtistStore } from "@store/useSelectedArtistStore";
-import { AxiosRequestConfig } from "axios";
-import axios from "axios";
 import { IFoundArtists } from "interfaces/foundArtist";
-import { useEffect } from "react";
 
 const FoundArtists = ({ foundArtists }: IFoundArtists) => {
-  const { setArtistUrl, artistUrl, setAlbumsUrls, albumsUrls } =
-    useSelectedArtistStore();
-
-  useEffect(() => {
-    const getAlbumsOptions: AxiosRequestConfig<any> = {
-      method: "GET",
-      url: "http://localhost:3001/albums",
-      params: { artistUrl },
-    };
-
-    const getAlbums = async () => {
-      await axios
-        .request(getAlbumsOptions)
-        .then((response): any => {
-          setAlbumsUrls(response.data);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    };
-
-    if (artistUrl) {
-      getAlbums().then(() => console.log("albumsUrls", albumsUrls));
-    }
-  }, [artistUrl, setAlbumsUrls, albumsUrls]);
+  const { getAlbums } = useGetArtistAlbums();
 
   return (
     <div className={styles.foundArtists}>
@@ -39,7 +11,7 @@ const FoundArtists = ({ foundArtists }: IFoundArtists) => {
         {foundArtists.map((artist) => (
           <li
             key={`${artist.name}_${artist.genre}`}
-            onClick={() => setArtistUrl(artist.url)}
+            onClick={() => getAlbums(artist.url)}
           >
             {artist.name}
           </li>
---
+++ src/hooks/useGetArtistAlbums.ts
@@ -0,0 +1,43 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useSelectedArtistStore } from "@store/useSelectedArtistStore";
+import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import { useState } from "react";
+
+const useGetArtistAlbums = () => {
+  const { setAlbumsUrls, setArtistUrl } = useSelectedArtistStore();
+  const [isAlbumsLoading, setIsAlbumsLoading] = useState(false);
+
+  // useEffect(() => {
+  //   if (!artistUrl || isAlbumsLoading) {
+  //     setShowLoader(true);
+  //     return;
+  //   }
+
+  //   setShowLoader(false);
+  // }, [isAlbumsLoading, artistUrl, setShowLoader]);
+
+  const getAlbums = async (artistUrl: string) => {
+    setIsAlbumsLoading(true);
+    setArtistUrl(artistUrl);
+
+    const getAlbumsOptions: AxiosRequestConfig<any> = {
+      method: "GET",
+      url: "http://localhost:3001/albums",
+      params: { artistUrl },
+    };
+
+    await axios
+      .request(getAlbumsOptions)
+      .then((response: AxiosResponse<any, any>) => {
+        setAlbumsUrls(response.data);
+      })
+      .finally(() => setIsAlbumsLoading(false))
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return { getAlbums };
+};
+
+export default useGetArtistAlbums;
