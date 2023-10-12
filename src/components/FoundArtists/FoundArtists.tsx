72fab37242be2b51cdb0885db17263504ed3bbcc
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-12 12:44:30 -0300

    feat: :sparkles: Added green play button to FoundArtists li elements.

--- src/components/FoundArtists/FoundArtists.tsx
+++ src/components/FoundArtists/FoundArtists.tsx
@@ -1,9 +1,15 @@
 import useGetArtistAlbums from "@hooks/useGetArtistAlbums";
 import styles from "./FoundArtists.module.scss";
 import { IFoundArtists } from "interfaces/foundArtist";
+import { BiPlay } from "react-icons/bi";
 
 const FoundArtists = ({ foundArtists }: IFoundArtists) => {
   const { getAlbums } = useGetArtistAlbums();
+  const iconStyle = {
+    color: "black",
+    fontSize: "2rem",
+    paddingLeft: "0.15rem",
+  };
 
   return (
     <div className={styles.foundArtists}>
@@ -20,6 +26,9 @@ const FoundArtists = ({ foundArtists }: IFoundArtists) => {
                 </picture>
                 <p className={styles.artistName}>{artist.name}</p>
                 <p className={styles.artistTag}>Artist</p>
+                <div className={styles.playButton}>
+                  <BiPlay style={iconStyle} />
+                </div>
               </div>
             </li>
           </>
