9f0f4208fdc6c7c75a99ccb668642833b58df7fb
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-12 08:23:18 -0300

    feat: :lipstick: Modifying styles.

--- src/components/FoundArtists/FoundArtists.tsx
+++ src/components/FoundArtists/FoundArtists.tsx
@@ -1,6 +1,7 @@
 import useGetArtistAlbums from "@hooks/useGetArtistAlbums";
 import styles from "./FoundArtists.module.scss";
 import { IFoundArtists } from "interfaces/foundArtist";
+import Separator from "@components/elements/Separator";
 
 const FoundArtists = ({ foundArtists }: IFoundArtists) => {
   const { getAlbums } = useGetArtistAlbums();
@@ -9,12 +10,15 @@ const FoundArtists = ({ foundArtists }: IFoundArtists) => {
     <div className={styles.foundArtists}>
       <ul>
         {foundArtists.map((artist) => (
-          <li
-            key={`${artist.name}_${artist.genre}`}
-            onClick={() => getAlbums(artist.url)}
-          >
-            {artist.name}
-          </li>
+          <>
+            <li
+              key={`${artist.name}_${artist.genre}`}
+              onClick={() => getAlbums(artist.url)}
+            >
+              {artist.name}
+            </li>
+            <Separator color="white" />
+          </>
         ))}
       </ul>
     </div>
