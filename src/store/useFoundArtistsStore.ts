bdc4e9ebde3a25f0961a1e6fa4f633300f185b16
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-13 08:33:55 -0300

    feat: :sparkles: Created useFoundArtistsStore.

---
+++ src/store/useFoundArtistsStore.ts
@@ -0,0 +1,22 @@
+import { IArtist } from "interfaces/artist";
+import { create } from "zustand";
+
+interface IFoundArtistsStore {
+  foundArtists: IArtist[] | null;
+  setFoundArtists: (foundArtists: IArtist[]) => void;
+  clearStore: () => void;
+}
+
+const initialState = {
+  foundArtists: null,
+};
+
+export const useLoaderStore = create<IFoundArtistsStore>()((set) => ({
+  ...initialState,
+  setFoundArtists: (foundArtists: IArtist[]) =>
+    set((state) => ({
+      ...state,
+      foundArtists,
+    })),
+  clearStore: () => set(() => ({ ...initialState })),
+}));
