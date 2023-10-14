6e68139a6c844bf657bad2f7e77d3ce3dd70445b
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-14 17:30:24 -0300

    feat: :sparkles: Modified ArtistInput.

--- src/components/SearchSection/components/ArtistInput/ArtistInput.tsx
+++ src/components/SearchSection/components/ArtistInput/ArtistInput.tsx
@@ -1,5 +1,6 @@
 import { IArtistInput } from "interfaces/artistInput";
 import styles from "./ArtistInput.module.scss";
+import SearchIcon from "@components/elements/Icons/SearchIcon";
 
 const ArtistInput = ({
   handleArtistFilterChange,
@@ -7,13 +8,18 @@ const ArtistInput = ({
   onKeyDown,
 }: IArtistInput) => {
   return (
-    <input
-      onChange={handleArtistFilterChange}
-      value={filteredArtist}
-      placeholder="Search Artist/Band"
-      onKeyDown={onKeyDown}
-      className={styles.artistInput}
-    />
+    <div className={styles.artistInput}>
+      <input
+        onChange={handleArtistFilterChange}
+        value={filteredArtist}
+        placeholder="Search Artist/Band"
+        onKeyDown={onKeyDown}
+        spellCheck="false"
+      />
+      <span className={styles.search}>
+        <SearchIcon />
+      </span>
+    </div>
   );
 };
 
