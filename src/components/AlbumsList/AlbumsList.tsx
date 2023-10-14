304ee534f7cdbabc2b86725615251d929b163006
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-14 11:31:11 -0300

    feat: :construction: Refactoring AlbumsList.

--- src/components/AlbumsList/AlbumsList.tsx
+++ src/components/AlbumsList/AlbumsList.tsx
@@ -18,39 +18,69 @@ const AlbumsList = ({ albums }: IAlbumsList) => {
 
   console.log("albums", albums);
 
+  const songs = albums.filter((album: IAlbum) => album.tracks.length === 1);
+  const fullAlbums = albums.filter((album: IAlbum) => album.tracks.length > 1);
+
   return (
     <div className={styles.albumsList}>
-      <h2>Albums</h2>
-      <ul>
-        {albums.map((album: IAlbum) => {
-          const handleAlbumOnClick = () => {
-            setTracks(album.tracks);
-            setShowTracks(true);
-            setAlbumArtist(album.artist.name);
-            setAlbumName(album.name);
-            setAlbumUrl(album.url);
-            setHideAlbums(true);
-            setAlbumImage(album.imageUrl);
-          };
+      {fullAlbums && (
+        <>
+          <h2>Albums</h2>
+          <ul>
+            {fullAlbums.map((album: IAlbum) => {
+              const handleAlbumOnClick = () => {
+                setTracks(album.tracks);
+                setShowTracks(true);
+                setAlbumArtist(album.artist.name);
+                setAlbumName(album.name);
+                setAlbumUrl(album.url);
+                setHideAlbums(true);
+                setAlbumImage(album.imageUrl);
+              };
 
-          const isOneTrack = album.tracks.length === 1;
+              return (
+                <ListedElement
+                  key={`${album.name}`}
+                  onClick={() => handleAlbumOnClick()}
+                  image={album.imageUrl}
+                  name={album.name}
+                  artist={album.artist.name}
+                  type={EListedElementTypes.ALBUM}
+                />
+              );
+            })}
+          </ul>
+        </>
+      )}
+      {songs && (
+        <>
+          <h2>Songs</h2>
+          <ul>
+            {songs.map((song: IAlbum) => {
+              const handleAlbumOnClick = () => {
+                setTracks(song.tracks);
+                setShowTracks(true);
+                setAlbumArtist(song.artist.name);
+                setAlbumName(song.name);
+                setAlbumUrl(song.url);
+                setHideAlbums(true);
+                setAlbumImage(song.imageUrl);
+              };
 
-          return (
-            <ListedElement
-              key={`${album.name}`}
-              onClick={() => handleAlbumOnClick()}
-              image={album.imageUrl}
-              name={album.name}
-              artist={album.artist.name}
-              type={
-                !isOneTrack
-                  ? EListedElementTypes.ALBUM
-                  : EListedElementTypes.TRACK
-              }
-            />
-          );
-        })}
-      </ul>
+              return (
+                <ListedElement
+                  key={`${song.name}`}
+                  onClick={() => handleAlbumOnClick()}
+                  image={song.imageUrl}
+                  name={song.name}
+                  artist={song.artist.name}
+                  type={EListedElementTypes.TRACK}
+                />
+              );
+            })}
+          </ul>
+        </>
+      )}
     </div>
   );
 };
