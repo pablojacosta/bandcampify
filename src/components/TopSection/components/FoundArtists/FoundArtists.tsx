1eb6473790134de34b3fac54455e5aca53757f20
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-15 01:00:19 -0300

    feat: :sparkles: Added artist not found message.

--- src/components/TopSection/components/FoundArtists/FoundArtists.tsx
+++ src/components/TopSection/components/FoundArtists/FoundArtists.tsx
@@ -6,21 +6,26 @@ import { EListedElementTypes } from "@constants/enums";
 
 const FoundArtists = ({ foundArtists }: IFoundArtists) => {
   const { getAlbums } = useGetArtistAlbums();
+  const isError = typeof foundArtists === "string";
 
   return (
     <div className={styles.foundArtists}>
       <h1>Artists</h1>
-      <ul>
-        {foundArtists.map((artist) => (
-          <ListedElement
-            key={`${artist.name}_${artist.genre}`}
-            onClick={() => getAlbums(artist.url)}
-            image={artist.imageUrl}
-            name={artist.name}
-            type={EListedElementTypes.ARTIST}
-          />
-        ))}
-      </ul>
+      {!isError ? (
+        <ul>
+          {foundArtists.map((artist) => (
+            <ListedElement
+              key={`${artist.name}_${artist.genre}`}
+              onClick={() => getAlbums(artist.url)}
+              image={artist.imageUrl}
+              name={artist.name}
+              type={EListedElementTypes.ARTIST}
+            />
+          ))}
+        </ul>
+      ) : (
+        <h2>{foundArtists}</h2>
+      )}
     </div>
   );
 };
