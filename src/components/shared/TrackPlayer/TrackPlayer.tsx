13c8c296241dfcdffece7f71ea06a03b3622d728
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-14 22:15:16 -0300

    feat: :lipstick: Added responsives to TrackPlayer.

--- src/components/shared/TrackPlayer/TrackPlayer.tsx
+++ src/components/shared/TrackPlayer/TrackPlayer.tsx
@@ -1,17 +1,26 @@
 import { useSelectedAlbumStore } from "@store/useSelectedAlbumStore";
 import styles from "./TrackPlayer.module.scss";
+import useMediaQuery from "@hooks/useMediaQuery";
 
 const TrackPlayer = () => {
   const { trackId } = useSelectedAlbumStore();
+  const isMobileBreakpoint = useMediaQuery(563);
+  const isSmallBreakpoint = useMediaQuery(416);
+  const isExtraSmallBreakpoint = useMediaQuery(330);
   const src = `https://bandcamp.com/EmbeddedPlayer/track=${trackId}/size=medium/bgcol=333333/linkcol=1ed760/artwork=false/transparent=true/`;
+
+  const style =
+    !isMobileBreakpoint && !isSmallBreakpoint && !isExtraSmallBreakpoint
+      ? { border: 0, width: 500, height: 150 }
+      : isMobileBreakpoint && !isSmallBreakpoint && !isExtraSmallBreakpoint
+      ? { border: 0, width: 400, height: 150 }
+      : isMobileBreakpoint && isSmallBreakpoint && !isExtraSmallBreakpoint
+      ? { border: 0, width: 300, height: 150 }
+      : { border: 0, width: 250, height: 150 };
 
   return (
     <div className={styles.trackPlayer}>
-      <iframe
-        style={{ border: 0, width: 500, height: 150 }}
-        src={src}
-        seamless
-      />
+      <iframe style={style} src={src} seamless />
     </div>
   );
 };
