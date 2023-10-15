88f837ae664d65abc63e442fe67421dda56d8c2f
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-14 21:07:18 -0300

    feat: :sparkles: Small change.

--- src/components/shared/TrackPlayer/TrackPlayer.tsx
+++ src/components/shared/TrackPlayer/TrackPlayer.tsx
@@ -3,7 +3,7 @@ import styles from "./TrackPlayer.module.scss";
 
 const TrackPlayer = () => {
   const { trackId } = useSelectedAlbumStore();
-  const src = `https://bandcamp.com/EmbeddedPlayer/track=${trackId}/size=medium/bgcol=333333/linkcol=1ed760/artwork=false//transparent=true/`;
+  const src = `https://bandcamp.com/EmbeddedPlayer/track=${trackId}/size=medium/bgcol=333333/linkcol=1ed760/artwork=false/transparent=true/`;
 
   return (
     <div className={styles.trackPlayer}>
@@ -11,7 +11,7 @@ const TrackPlayer = () => {
         style={{ border: 0, width: 500, height: 150 }}
         src={src}
         seamless
-      ></iframe>
+      />
     </div>
   );
 };
