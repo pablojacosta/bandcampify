a9ee4f4fc1c673e489179a545392f334c63cdd6f
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-14 22:41:17 -0300

    feat: :lipstick: Added responsives to SearchArtistButton.

--- src/components/SearchSection/components/SearchArtistButton/SearchArtistButton.tsx
+++ src/components/SearchSection/components/SearchArtistButton/SearchArtistButton.tsx
@@ -1,7 +1,12 @@
 import { ISearchArtistButton } from "interfaces/searchArtistButton";
+import styles from "./SearchArtistButton.module.scss";
 
 const SearchArtistButton = ({ getArtistData }: ISearchArtistButton) => {
-  return <button onClick={getArtistData}>Search!</button>;
+  return (
+    <button onClick={getArtistData} className={styles.searchArtistButton}>
+      Search!
+    </button>
+  );
 };
 
 export default SearchArtistButton;
