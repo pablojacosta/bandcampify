799ecae83d22d49cb1f3393781eb57bc4f99e85d
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-17 12:17:28 -0300

    feat: :recycle: Created TrackListHeader component.

--- src/components/TrackList/TrackList.tsx
+++ src/components/TrackList/TrackList.tsx
@@ -5,9 +5,10 @@ import { useSelectedAlbumStore } from "@store/useSelectedAlbumStore";
 import TrackPlayer from "@components/shared/TrackPlayer";
 import { getTrackId } from "@utils/helpers/getTrackId";
 import { formatDuration } from "@utils/helpers/formatDuration";
-import ClockIcon from "@components/elements/Icons/ClockIcon";
+
 import GoBackButton from "./components/GoBackButton";
 import TrackListTop from "./components/TrackListTop";
+import TrackListHeader from "./components/TrackListHeader";
 
 const TrackList = ({ tracks, albumId }: ITrackList) => {
   const { setShowPlayer, showPlayer, setTrackId } = useSelectedAlbumStore();
@@ -20,15 +21,7 @@ const TrackList = ({ tracks, albumId }: ITrackList) => {
     <div className={styles.trackList}>
       <GoBackButton />
       <TrackListTop />
-      <div className={styles.separator}>
-        <div className={styles.left}>
-          <p className={styles.trackNumber}>#</p>
-          <p className={styles.title}>Title</p>
-        </div>
-        <div className={styles.clock}>
-          <ClockIcon />
-        </div>
-      </div>
+      <TrackListHeader />
       <ul>
         {tracks.map((track, index) => (
           <li key={`${albumId}_${track.name}`}>
