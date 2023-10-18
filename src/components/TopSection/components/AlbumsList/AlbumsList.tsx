372c891a8a1f47ed4dc129ab18fd6b66b6d70d8e
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-18 14:11:48 -0300

    feat: :coffin: Removed commented code.

--- src/components/TopSection/components/AlbumsList/AlbumsList.tsx
+++ src/components/TopSection/components/AlbumsList/AlbumsList.tsx
@@ -8,12 +8,6 @@ const AlbumsList = () => {
   const albums = artistInfo?.albums;
   const fullAlbums = albums?.filter((album) => album.url.includes("album"));
   const songs = albums?.filter((album) => album.url.includes("track"));
-  // const songs = albums.filter((album: IAlbum) => album.tracks.length === 1);
-  // const sanitizedSongs = songs.filter((obj, index) => {
-  //   return index === songs.findLastIndex((o) => obj.name === o.name);
-  // });
-  // const fullAlbums = albums.filter((album: IAlbum) => album.tracks.length > 1);
-
   const hasAlbums = fullAlbums && fullAlbums.length > 0;
   const hasSongs = songs && songs.length > 0;
 
