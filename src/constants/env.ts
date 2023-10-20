34c877be7f9a1bfc34b280b122b065eeee27435d
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-20 09:58:35 -0300

    fix: :bug: Fixed env.ts.

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,9 +1,9 @@
-export const FB_APIKEY = import.meta.env.REACT_APP_FB_APIKEY;
-export const FB_AUTH_DOMAIN = import.meta.env.REACT_APP_FB_AUTH_DOMAIN;
-export const FB_DB_URL = import.meta.env.REACT_APP_FB_DB_URL;
-export const FB_PROJECT_ID = import.meta.env.REACT_APP_FB_PROJECT_ID;
-export const FB_STORAGE_BUCKET = import.meta.env.REACT_APP_FB_STORAGE_BUCKET;
+export const FB_APIKEY = import.meta.env.VITE_FB_APIKEY;
+export const FB_AUTH_DOMAIN = import.meta.env.VITE_FB_AUTH_DOMAIN;
+export const FB_DB_URL = import.meta.env.VITE_FB_DB_URL;
+export const FB_PROJECT_ID = import.meta.env.VITE_FB_PROJECT_ID;
+export const FB_STORAGE_BUCKET = import.meta.env.VITE_FB_STORAGE_BUCKET;
 export const FB_MESSAGING_SENDER_ID = import.meta.env
-  .REACT_APP_FB_MESSAGING_SENDER_ID;
-export const FB_APP_ID = import.meta.env.REACT_APP_FB_APP_ID;
-export const FB_MEASUREMENT_ID = import.meta.env.REACT_APP_FB_MEASUREMENT_ID;
+  .VITE_FB_MESSAGING_SENDER_ID;
+export const FB_APP_ID = import.meta.env.VITE_FB_APP_ID;
+export const FB_MEASUREMENT_ID = import.meta.env.VITE_FB_MEASUREMENT_ID;
