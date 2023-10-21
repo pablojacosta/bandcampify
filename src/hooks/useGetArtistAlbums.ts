68fb1f7f911a05d52d47e6e83928e152a13c864a
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-21 13:33:59 -0300

    feat: :sparkles: Modified useGetArtistAlbums hook,

--- src/hooks/useGetArtistAlbums.ts
+++ src/hooks/useGetArtistAlbums.ts
@@ -5,13 +5,19 @@ import { useSelectedArtistStore } from "@store/useSelectedArtistStore";
 import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
 
 const useGetArtistAlbums = () => {
-  const { setArtistUrl, setHideArtists, setArtistImage, setArtistInfo } =
-    useSelectedArtistStore();
+  const {
+    setArtistUrl,
+    setHideArtists,
+    setArtistImage,
+    setArtistInfo,
+    setFetchArtist,
+  } = useSelectedArtistStore();
   const { setHideAlbums } = useSelectedAlbumStore();
   const { setShowLoader } = useLoaderStore();
 
   const getAlbums = async (artistUrl: string, artistImage: string) => {
     setShowLoader(true);
+    setFetchArtist(false);
     setArtistUrl(artistUrl);
     setHideArtists(true);
     setArtistImage(artistImage);
