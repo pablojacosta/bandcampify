357f54025080c3cff26b37bb2871a612dc5998b8
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-21 03:56:21 -0300

    feat: :sparkles: Modified FoundResults.

--- src/components/TopSection/components/FoundResults/FoundResults.tsx
+++ src/components/TopSection/components/FoundResults/FoundResults.tsx
@@ -2,33 +2,87 @@ import useGetArtistAlbums from "@hooks/useGetArtistAlbums";
 import styles from "./FoundResults.module.scss";
 import { IFoundResults } from "interfaces/foundResult";
 import ListedElement from "@components/shared/ListedElement";
-import { EListedElementTypes } from "@constants/enums";
+import { EListedElementTypes, ESearchResultTypes } from "@constants/enums";
 
 const FoundResults = ({ foundResults }: IFoundResults) => {
   const { getAlbums } = useGetArtistAlbums();
   const isError = typeof foundResults === "string";
+  const artists = foundResults.filter(
+    (result) => result.type === ESearchResultTypes.ARTIST
+  );
+  const albums = foundResults.filter(
+    (result) => result.type === ESearchResultTypes.ALBUM
+  );
+  const tracks = foundResults.filter(
+    (result) => result.type === ESearchResultTypes.TRACK
+  );
+  const hasArtists = artists.length > 0;
+  const hasAlbums = albums.length > 0;
+  const hasTracks = tracks.length > 0;
 
   console.log("foundResults", foundResults);
 
   return (
     <div className={styles.foundResults}>
-      <h1>Artists</h1>
       {!isError ? (
-        <ul>
-          {foundResults
-            .filter((result) => result.type === "artist")
-            .map((artist) => (
-              <ListedElement
-                key={`${artist.name}_${artist.genre}`}
-                onClick={() => getAlbums(artist.url, artist.imageUrl)}
-                image={artist.imageUrl}
-                name={artist.name}
-                type={EListedElementTypes.ARTIST}
-              />
-            ))}
-        </ul>
+        <>
+          {hasArtists && (
+            <>
+              <h1>Artists</h1>
+              <ul>
+                {foundResults
+                  .filter((result) => result.type === ESearchResultTypes.ARTIST)
+                  .map((artist) => (
+                    <ListedElement
+                      key={`${artist.name}_${artist.genre}`}
+                      onClick={() => getAlbums(artist.url, artist.imageUrl)}
+                      image={artist.imageUrl}
+                      name={artist.name}
+                      type={EListedElementTypes.ARTIST}
+                    />
+                  ))}
+              </ul>
+            </>
+          )}
+          {hasAlbums && (
+            <>
+              <h1>Albums</h1>
+              <ul>
+                {foundResults
+                  .filter((result) => result.type === ESearchResultTypes.ALBUM)
+                  .map((album) => (
+                    <ListedElement
+                      key={album.url}
+                      onClick={() => getAlbums(album.url, album.imageUrl)}
+                      image={album.imageUrl}
+                      name={album.name}
+                      type={EListedElementTypes.ALBUM}
+                    />
+                  ))}
+              </ul>
+            </>
+          )}
+          {hasTracks && (
+            <>
+              <h1>Songs</h1>
+              <ul>
+                {foundResults
+                  .filter((result) => result.type === ESearchResultTypes.TRACK)
+                  .map((song) => (
+                    <ListedElement
+                      key={song.url}
+                      onClick={() => getAlbums(song.url, song.imageUrl)}
+                      image={song.imageUrl}
+                      name={song.name}
+                      type={EListedElementTypes.ALBUM}
+                    />
+                  ))}
+              </ul>
+            </>
+          )}
+        </>
       ) : (
-        <h2>{foundResults}</h2>
+        <h2>Sorry. No results found. Please, try again.</h2>
       )}
     </div>
   );
