1fcccac4cf48eeb391b02a694bd89af3b55ad6a0
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-22 08:25:51 -0300

    feat: :lipstick: Created HorizontalLoader.

---
+++ src/components/shared/HorizontalLoader/HorizontalLoader.tsx
@@ -0,0 +1,7 @@
+import styles from "./HorizontalLoader.module.scss";
+
+const HorizontalLoader = () => {
+  return <div className={styles.horizontalLoader}></div>;
+};
+
+export default HorizontalLoader;
