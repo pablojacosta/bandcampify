01d46817b7014ba90407b23200e51277610c9736
Pablo Acosta <pablo.acosta@nftstudios.io>
2023-10-22 08:08:39 -0300

    feat: :sparkles: Added Loader to AlbumsList.

--- src/components/TopSection/components/AlbumsList/AlbumsList.tsx
+++ src/components/TopSection/components/AlbumsList/AlbumsList.tsx
@@ -3,21 +3,41 @@ import { EListType } from "@constants/enums";
 import List from "./components/List";
 import { useSelectedArtistStore } from "@store/useSelectedArtistStore";
 import GoBackButton from "@components/shared/GoBackButton";
+import { useLoaderStore } from "@store/useLoaderStore";
+import { useEffect } from "react";
+import Loader from "@components/shared/Loader";
 
 const AlbumsList = () => {
   const { artistInfo } = useSelectedArtistStore();
+  const { showLoader, setShowLoader } = useLoaderStore();
   const albums = artistInfo?.albums;
   const fullAlbums = albums?.filter((album) => album.url.includes("album"));
   const songs = albums?.filter((album) => album.url.includes("track"));
   const hasAlbums = fullAlbums && fullAlbums.length > 0;
   const hasSongs = songs && songs.length > 0;
 
+  useEffect(() => {
+    setShowLoader(true);
+    setTimeout(() => setShowLoader(false), 2000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <div className={styles.albumsList}>
-      <GoBackButton isAlbums />
-      {hasAlbums && <List items={fullAlbums} type={EListType.ALBUMS} />}
-      {hasSongs && <List items={songs} type={EListType.SONGS} />}
-    </div>
+    <>
+      <div className={styles.albumsList}>
+        {!showLoader ? (
+          <>
+            <GoBackButton isAlbums />
+            {hasAlbums && <List items={fullAlbums} type={EListType.ALBUMS} />}
+            {hasSongs && <List items={songs} type={EListType.SONGS} />}
+          </>
+        ) : (
+          <div className={styles.loader}>
+            <Loader />
+          </div>
+        )}
+      </div>
+    </>
   );
 };
 
