a9012b79e2cb9dd374a92fa7e147ca5de9562e77
Pablo Acosta <pablo.acosta@nftstudios.io>
2024-01-07 10:51:40 -0300

    feat: :sparkles: Added facebook icon. Removed linkedin.

--- src/components/shared/Footer/components/SocialIcons/SocialIcons.tsx
+++ src/components/shared/Footer/components/SocialIcons/SocialIcons.tsx
@@ -1,6 +1,6 @@
-import { githubUrl, linkedinUrl } from "constants/socials";
+import { facebookUrl, githubUrl } from "constants/socials";
 import githubIcon from "@images/github-icon.svg";
-import linkedinIcon from "@images/linkedin-icon.svg";
+import facebookIcon from "@images/facebook-icon.svg";
 import styles from "./SocialIcons.module.scss";
 import HeartIcon from "@components/elements/Icons/HeartIcon";
 import useGetLikes from "@hooks/useGetLikes";
@@ -8,7 +8,7 @@ import useSetLikes from "@hooks/useSetLikes";
 import { useLikesStore } from "@store/useLikesStore";
 import CoffeButton from "../CoffeButton";
 
-const LINKEDIN_ICON_SIZE = "50";
+const FACEBOOK_ICON_SIZE = "52";
 const GITHUB_ICON_SIZE = "42";
 
 interface ISocialIcons {
@@ -34,14 +34,14 @@ const SocialIcons = ({ className }: ISocialIcons) => {
           />
         </picture>
       </a>
-      <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+      <a href={facebookUrl} target="_blank" rel="noopener noreferrer">
         <picture>
           <img
-            src={linkedinIcon}
-            alt="Linkedin Icon"
-            className={styles.linkedinIcon}
-            width={LINKEDIN_ICON_SIZE}
-            height={LINKEDIN_ICON_SIZE}
+            src={facebookIcon}
+            alt="Facebook Icon"
+            className={styles.facebookIcon}
+            width={FACEBOOK_ICON_SIZE}
+            height={FACEBOOK_ICON_SIZE}
           />
         </picture>
       </a>
